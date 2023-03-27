2c16fbcb62bb18c97c04b2869d980e8d38d4334b
Igor Santos de Souza <63150786+IgorSouza4489@users.noreply.github.com>
2023-03-27 17:18:23 -0300

    subi dash
    s

--- pb/src/components/DashBoard.vue
+++ pb/src/components/DashBoard.vue
@@ -1,7 +1,37 @@
 <template>
     <div>
       <header-home :notificacoes="notificacoes" :click-logout="logout" ></header-home>
-        <h1>Olá {{ user }}</h1>
+      <h1>Olá {{ user }}</h1>
+      <div class="table-responsive">
+        <table class="table table-striped">
+      <thead>
+        <tr>
+          <th class=" col-sm-2">Nome</th> <!--Esconde em mobile-->
+          <th class="  col-sm-2">Idade</th>
+
+          <th class="d-none d-sm-table-cell col-sm-2">Sexo</th> <!--Esconde em mobile-->
+          <th class="col-sm-2 ">Convênio</th> 
+          <th class="col-sm-2"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(paciente, idpaciente) in pacientes" :key="idpaciente"  >
+          <td class=" col-sm-2">{{ paciente.nome }}</td> <!--Esconde em mobile-->
+          <td class=" col-sm-2">{{ paciente.idade }}</td> <!--Esconde em mobile-->
+          <td class="d-none d-sm-table-cell  col-sm-2">{{ paciente.sexo }}</td>
+
+          <td style="font-weight:bold" class="col-sm-2">{{ paciente.convenio }}</td>
+          <td class="col-sm-2"><button id="btndetalhes" @click="verDetalhes(idpaciente)">Ver Detalhes</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div  v-if="pacienteSelecionado !== null">
+    <modal-paciente v-show="mostrarModal"
+        :pacienteSelecionado="pacienteSelecionado"
+        @close="mostrarModal = false"
+      ></modal-paciente>
+  </div>
     </div>
 </template>
 
@@ -9,6 +39,8 @@
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
 import app from "../../firebase"
 import HeaderHome from './header/HeaderHome'
+import ModalPaciente from "./modal/ModalPaciente.vue";
+
 
 const auth = getAuth(app)
 
@@ -16,10 +48,12 @@ const auth = getAuth(app)
 export default {
   components: {
       HeaderHome,
-      
+      ModalPaciente
     },
 data() {
     return{
+      mostrarModal: false,
+      pacienteSelecionado: null,
         user: null,
         error : null,
         notificacoes: [
@@ -27,6 +61,23 @@ data() {
               { id: 2, mensagem: "Notificação 2" },
               { id: 3, mensagem: "Notificação 3" }
             ],
+            pacientes: [
+        {
+          id: 1,
+          nome: "Elon Musk",
+          idade: "99",
+          sexo: "Masculino",
+          convenio: "Aprovada"
+        },
+        {
+          id: 2,
+          nome: "Bill Gates",
+          idade: "49",
+          sexo: "Masculino",
+          convenio: "Unimed"
+        },
+      ]
+            
     }
 },
 created() {
@@ -40,8 +91,10 @@ created() {
       })
     },
 methods: {
-    register(){
-        
+
+    verDetalhes(index) {
+        this.mostrarModal = true;
+        this.pacienteSelecionado = this.pacientes[index];
     },
 
     logout(){
@@ -57,5 +110,19 @@ methods: {
 </script>
 
 <style>
+#btndetalhes{
+  background-color: transparent;
+  color: #38b6ff;
+  border: 1px solid #38b6ff;
+  padding: 5px;
+  border-radius: 20px
+}
 
+#btndetalhes:hover{
+  background-color: transparent;
+  color: #0069a7;
+  border: 1px solid #0069a7;
+  padding: 5px;
+  border-radius: 20px
+}
 </style>
