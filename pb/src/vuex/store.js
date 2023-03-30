8251f6dfb998295e5572e08fe8ee0f70cc6dd3f8
Igor Santos de Souza <63150786+IgorSouza4489@users.noreply.github.com>
2023-03-30 18:46:56 -0300

    Update store.js

--- pb/src/vuex/store.js
+++ pb/src/vuex/store.js
@@ -5,28 +5,28 @@ import api from "../api";
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {
-    data: []
-  },
-  getters: {
-    getData: state => state.data
-  },
-  mutations: {
-    setData(state, data) {
-      state.data = data
-    }
-  },
-  actions: {
-    async getUserData() {
-        try {
-          const response = await api.get(`/remedios `);
-          this.remedios = response.data;
-          console.log(this.remedios)
-        } catch (error) {
-          console.error(error);
+    state: {
+        data: []
+    },
+    getters: {
+        getData: state => state.data
+    },
+    mutations: {
+        setData(state, data) {
+            state.data = data
         }
-      },
-  }
+    },
+    actions: {
+        async getUserData() {
+            try {
+                const response = await api.get(`/remedios `);
+                this.remedios = response.data;
+                console.log(this.remedios)
+            } catch (error) {
+                console.error(error);
+            }
+        },
+    }
 })
 
 export default store
